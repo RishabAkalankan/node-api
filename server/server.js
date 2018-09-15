2373cd1ea90e07731d583abb334831e52a47c0d2
rishab <rishabkumara@gmail.com>
2018-09-15 17:33:36 +0530

    Added GET users/me

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ var { ObjectID } = require('mongodb');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+var { authenticate } = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -116,6 +117,11 @@ app.post('/users' , (request, response) => {
     }).catch((e)=> {
         response.status(400).send({e});
     });
+});
+
+
+app.get('/users/me',authenticate, (request, response)=> {
+    response.send(request.user);
 });
 
 
