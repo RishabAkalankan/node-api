dcd17607e51801f08e9398e6119fb4d5b562d787
rishab <rishabkumara@gmail.com>
2018-09-15 20:30:24 +0530

    Hashed user pwd

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const _ = require('lodash');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
     email: {
@@ -71,6 +72,21 @@ UserSchema.statics.findByToken = function (token) {
         'tokens.access': 'auth'
     });
 }
+
+UserSchema.pre('save', function (next) {
+    var user = this;
+    if(user.isModified('password')) {
+        bcrypt.genSalt(10).then((salt) => {
+            bcrypt.hash(user.password, salt).then((hash)=> {
+                user.password = hash;
+                next();
+            });
+        })
+    } else {
+        next();
+    }
+
+})
 
 var User = mongoose.model('User', UserSchema);
 
